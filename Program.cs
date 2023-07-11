aea1a550c29f796a36048de02dc1e6098f2f466e
Vladimir <voiager@rambler.ru>
2023-07-11 15:29:03 +0300

    Создал два метода Заполнение массива и Печать массива в коде по задаче

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿// Написать программу, которая из имеющегося массива строк формирует новый массив из строк,
+// длина которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры,
+//  либо задать на старте выполнения алгоритма.
+// При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+
+// Объявление переменных инициализация массивов
+string[] A = new string[4];
+string[] B = new string[4];
+
+void FillArray(string []A)
+{
+    for (int i = 0; i < A.Length; i++)
+    {
+        string  str = System.String.Empty;
+        Console.WriteLine("Введите строку");
+        str = Console.ReadLine();
+        A[i] = str;
+    }
+}
+void PrintArray(string []A)
+{
+    string str = System.String.Empty;
+    for (int i = 0; i < A.Length; i++)
+    {
+        str = string.Join(" ", A);
+        Console.WriteLine(str);
+        
+    }
+}
+
+FillArray(A);
+PrintArray(A);
